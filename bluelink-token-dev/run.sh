cb2368b3a2854784e0b049e78fdf316217002f59
Tobias Malcherek <tobias.malcherek@gmail.com>
2026-04-29 20:11:53 +0200

    feat: make API_TOKEN configurable in Home Assistant addon settings

--- bluelink-token-dev/run.sh
+++ bluelink-token-dev/run.sh
@@ -2,6 +2,7 @@
 
 EVCC_URL=""
 EVCC_PASSWORD=""
+API_TOKEN=""
 
 if bashio::config.has_value 'country'; then
     export COUNTRY=$(bashio::config 'country')
@@ -12,12 +13,16 @@ fi
 if bashio::config.has_value 'evcc_password'; then
     EVCC_PASSWORD=$(bashio::config 'evcc_password')
 fi
+if bashio::config.has_value 'api_token'; then
+    API_TOKEN=$(bashio::config 'api_token')
+fi
 
 # Build vehicles JSON from config
 VEHICLES_JSON=$(bashio::config 'vehicles')
 export VEHICLES_JSON
 export EVCC_URL
 export EVCC_PASSWORD
+export API_TOKEN
 
 bashio::log.info "Starting Bluelink Token Generator..."
 vehicle_count=$(echo "$VEHICLES_JSON" | python3 -c "import sys,json; print(len(json.loads(sys.stdin.read())))" 2>/dev/null || echo "0")
